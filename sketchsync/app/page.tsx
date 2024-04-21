e691dd24ccced65ffbe98aceec207c603dbde857
GANSTA-OG <ojaswisharma2013.study@gmail.com>
2024-04-21 23:13:54 +0530

    upto clerk auth and convex addition adding some info
    Backend imiplenettion using convex
    Two command to run the project
    -npm run dev
    -npx convex dev
    
    //iski aur humari middleware file main change hai
    authMiddleware() has been depriciated so hume use kiya hai clerkMiddleware
    
    --->neeche hai middleware.ts ka code
    import { clerkMiddleware } from '@clerk/nextjs/server';
    
    export default clerkMiddleware()
    
    export const config = {
      matcher: [
        '/((?!.*\\..*|_next).*)', // Don't run middleware on static files
        '/', // Run middleware on index page
        '/(api|trpc)(.*)'], // Run middleware on API routes
    };

--- sketchsync/app/page.tsx
+++ sketchsync/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
    <Button>
-    click
+    click ME :smiles
    </Button>
   );
 }
---
+++ sketchsync/convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://actual-bass-34.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
